0311e97c667809610e828132d696fd6a913cbae9
Doan Son Tung <doansontung1986@192.168.2.10>
2024-08-23 11:35:17 +0700

    First commit

---
+++ src/test/java/com/vinsguru/tests/flightreservation/FlightReservationTest.java
@@ -0,0 +1,66 @@
+package com.vinsguru.tests.flightreservation;
+
+import com.vinsguru.pages.flightreservation.*;
+import com.vinsguru.tests.AbstractTest;
+import com.vinsguru.tests.flightreservation.model.FlightReservationTestData;
+import com.vinsguru.util.Config;
+import com.vinsguru.util.Constants;
+import com.vinsguru.util.JsonUtil;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class FlightReservationTest extends AbstractTest {
+
+    private FlightReservationTestData testData;
+
+    @BeforeTest
+    @Parameters("testDataPath")
+    public void setParameters(String testDataPath) {
+        this.testData = JsonUtil.getTestData(testDataPath, FlightReservationTestData.class);
+    }
+
+    @Test
+    public void userReservationTest() {
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.goTo(Config.get(Constants.FLIGHT_RESERVATION_URL));
+        Assert.assertTrue(registrationPage.isAt());
+
+        registrationPage.enterUserDetails(testData.firstName(), testData.lastName());
+        registrationPage.enterUserCredentials(testData.email(), testData.password());
+        registrationPage.enterAddress(testData.street(), testData.city(), testData.zip());
+        registrationPage.register();
+    }
+
+    @Test(dependsOnMethods = "userReservationTest")
+    public void registrationConfirmationTest() {
+        RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
+        Assert.assertTrue(registrationConfirmationPage.isAt());
+        registrationConfirmationPage.goToFlightSearch();
+    }
+
+    @Test(dependsOnMethods = "registrationConfirmationTest")
+    public void flightSearchTest() {
+        FlightsSearchPage flightsSearchPage = new FlightsSearchPage(driver);
+        Assert.assertTrue(flightsSearchPage.isAt());
+        flightsSearchPage.selectPassengers(testData.passengersCount());
+        flightsSearchPage.searchFlights();
+    }
+
+    @Test(dependsOnMethods = "flightSearchTest")
+    public void flightsSelectionTest() {
+        FlightsSelectionPage flightsSelectionPage = new FlightsSelectionPage(driver);
+        Assert.assertTrue(flightsSelectionPage.isAt());
+        flightsSelectionPage.selectFlights();
+        flightsSelectionPage.confirmFlights();
+    }
+
+    @Test(dependsOnMethods = "flightsSelectionTest")
+    public void flightReservationConfirmationTest() {
+        FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
+        Assert.assertTrue(flightConfirmationPage.isAt());
+        Assert.assertEquals(flightConfirmationPage.getPrice(), testData.expectedPrice());
+    }
+
+}
---
+++ src/test/java/com/vinsguru/tests/vendorportal/VendorPortalTest.java
@@ -0,0 +1,54 @@
+package com.vinsguru.tests.vendorportal;
+
+import com.vinsguru.pages.vendorportal.DashboardPage;
+import com.vinsguru.pages.vendorportal.LoginPage;
+import com.vinsguru.tests.AbstractTest;
+import com.vinsguru.tests.vendorportal.model.VendorPortalTestData;
+import com.vinsguru.util.Config;
+import com.vinsguru.util.Constants;
+import com.vinsguru.util.JsonUtil;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VendorPortalTest extends AbstractTest {
+
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+    private VendorPortalTestData testData;
+
+    @BeforeTest
+    @Parameters("testDataPath")
+    public void setPageObjects(String testDataPath) {
+        this.loginPage = new LoginPage(this.driver);
+        this.dashboardPage = new DashboardPage(this.driver);
+        this.testData = JsonUtil.getTestData(testDataPath, VendorPortalTestData.class);
+    }
+
+    @Test
+    public void loginTest() {
+        loginPage.goTo(Config.get(Constants.VENDOR_PORTAL_URL));
+        Assert.assertTrue(loginPage.isAt());
+        loginPage.login(testData.username(), testData.password());
+    }
+
+    @Test(dependsOnMethods = "loginTest")
+    public void dashboardTest() {
+        Assert.assertTrue(dashboardPage.isAt());
+
+        Assert.assertEquals(dashboardPage.getMonthlyEarning(), testData.monthlyEarning());
+        Assert.assertEquals(dashboardPage.getAnnualEarning(), testData.annualEarning());
+        Assert.assertEquals(dashboardPage.getProfitMargin(), testData.profitMargin());
+        Assert.assertEquals(dashboardPage.getAvailableInventory(), testData.availableInventory());
+
+        dashboardPage.searchOrderHistoryBy(testData.searchKeyword());
+        Assert.assertEquals(dashboardPage.getSearchResultsCount(), testData.searchResultsCount());
+    }
+
+    @Test(dependsOnMethods = "dashboardTest")
+    public void logoutTest() {
+        dashboardPage.logout();
+        Assert.assertTrue(loginPage.isAt());
+    }
+}
